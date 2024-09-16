847745461b2b4a34eeca657c64cda5a3ddaf0940
velladuraiVD <velladurai3007@gmail.com>
2024-09-16 16:12:13 +0530

    logo change

--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -4,24 +4,24 @@ import { SocialIcon } from 'react-social-icons'
 
 export default function ContactCard() {
     return (
-        <div className='bg-red-600 flex flex-col items-center  md:flex-row justify-between px-7'>
+        <div className='bg-black flex flex-col items-center  md:flex-row justify-between px-7 '>
             <div className= 'flex flex-col justify-center items-center md:flex-row  '>
                 <div className='flex items-center'>
-                    <SocialIcon bgColor='#DC2626' fgColor='white' style={{height:"45px"}} url="www.whatsapp.com" />
-                    <p className='text-white text-xl hover:text-[#2b4258] cursor-pointer'>+918475845739</p>
+                    <SocialIcon bgColor='black' fgColor='white' style={{height:"45px"}} url="www.whatsapp.com" />
+                    <p className='text-white text-xl hover:text-[#469173] transition-all ease-in duration-300 cursor-pointer'>+918475845739</p>
                 </div>
                 <vr className=' hidden md:block w-[0.3px] h-5 bg-white mx-5' />
                 <div className=' flex  md:flex-row items-center'>
-                <SocialIcon bgColor= "#DC2626" fgColor='white' style={{height:"45px"}} url="www.email.com" />
-                    <p className='text-white text-xl hover:text-[#2b4258]'>Name@gmail.com</p>
+                <SocialIcon bgColor= "black" fgColor='white' style={{height:"45px"}} url="www.email.com" />
+                    <p className='text-white text-xl  hover:text-[#469173] transition-all ease-in duration-300 cursor-pointer'>Name@gmail.com</p>
                 </div>
             </div>
 
             <div className=' hidden md:flex' >
-                <SocialIcon bgColor='#DC2626' fgColor='white' url="www.facebook.com" />
-                <SocialIcon bgColor='#DC2626' fgColor='white' url="www.twitter.com" />
-                <SocialIcon bgColor='#DC2626' fgColor='white' url="www.linkedin.com" />
-                <SocialIcon bgColor='#DC2626' fgColor='white' url="www.instagram.com" />
+                <SocialIcon bgColor='black' fgColor='white' url="www.facebook.com" />
+                <SocialIcon bgColor='black' fgColor='white' url="www.twitter.com" />
+                <SocialIcon bgColor='black' fgColor='white' url="www.linkedin.com" />
+                <SocialIcon bgColor='black' fgColor='white' url="www.instagram.com" />
             </div>
         </div>
     )
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
-import logo from "./Assets/karaAi logo2.png"
+import logo from "./Assets/newlogo.png"
+import { IoIosMenu } from "react-icons/io";
 
 export default function NavBar() {
     return (
-        <div className='bg-black flex justify-around items-center'>
-            <div>
-                <img src={logo} alt="img" className='h-[65px] mt-3 hover:text-gray-950' />
+        <div className='bg-white lg:flex-row   xl:flex justify-around items-center flex-wrap sticky top-0 '>
+            <div className='flex justify-center items-center gap-3 mt-3 flex-wrap' >
+                <img src={logo} alt="img" className='h-[50px] mt-3 hover:text-gray-950' />
+                <div className='flex-row justify-center  xl:flex-col '>
+                    <h1 className='text-2xl font-bold'>Kar Ai Solutions </h1>
+                    <h1 className=' md:block text-2xl font-bold' >Private Limited (PVT LTD)</h1>
+                </div>
             </div>
-            <div >
+            <div>
+                <IoIosMenu className='bg-yellow-500 text-white  text-3xl block absolute right-0 top-[16%] m-3 md:hidden' />
+            </div>
+
+            <div className='hidden md:flex mt-10 mx-8 justify-center items-center flex-wrap xl:mt-0' >
                 <form >
-                    <ul className='flex gap-7 '>
-                        <li className='text-xl hover:text-red-600'>Home</li>
-                        <li className='text-xl hover:text-red-600'>About Us</li>
-                        <li className='text-xl hover:text-red-600'>Cloud Apllications</li>
-                        <li className='text-xl hover:text-red-600'>Service & Support</li>
-                        <li className='text-xl hover:text-red-600'>Industries</li>
-                        <li className='text-xl hover:text-red-600'>Contact Us</li>
+                    <ul className='flex gap-7 flex-wrap '>
+                        <li className='text-xl font-semibold text-black hover:text-red-600 md:tex'>Home</li>
+                        <li className='text-xl font-semibold text-black hover:text-red-600'>About Us</li>
+                        <li className='text-xl font-semibold text-black hover:text-red-600'>Cloud Apllications </li>
+                        <li className='text-xl font-semibold text-black hover:text-red-600'>Service & Support</li>
+                        <li className='text-xl font-semibold text-black hover:text-red-600'>Industries</li>
+                        <li className='text-xl font-semibold text-black hover:text-red-600'>Contact Us</li>
                     </ul>
                 </form>
             </div>
+            
         </div>
     )
 }
